ad247d6f5e77caa0518d4dd7633c598eae8f80c3
eshifflett <ethan.shiffy@gmail.com>
2022-06-17 23:33:08 -0400

    Quick fix commit

--- app/src/main/java/com/example/cashgametracker/MainActivity.kt
+++ app/src/main/java/com/example/cashgametracker/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.cashgametracker
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var newSessionButton: Button
     private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +24,13 @@ class MainActivity : AppCompatActivity() {
         if(supportActionBar != null){
             this.supportActionBar?.hide()
         }
+
+        initializeUI()
+    }
+
+    // Function for UI initialization
+    private fun initializeUI() {
+
     }
 
     override fun onStart(){
@@ -33,5 +43,11 @@ class MainActivity : AppCompatActivity() {
             finish()
             return
         }
+    }
+
+    // Listener function for new session button
+    fun onNewSessionPress(view: View) {
+        // Launching new session activity
+        startActivity(Intent(this, NewSessionActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/example/cashgametracker/NewSessionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cashgametracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewSessionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_session)
+    }
+}
